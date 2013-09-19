71477612df7dc550b924ec93bf99434891b2579e
Janie Gu <janiegu.x@gmail.com>
2013-09-19 00:56:44 -0400

    Wrote shell script to download shapefile from U.S. Census Bureau, unzip it, convert it to GeoJSON, and then convert it to a TopoJSON file mapped to zip code

---
+++ topoScript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                                                                       
+
+mkdir -p geojson topojson
+
+for (( i=1; i <= 78; i++ ))
+do
+    if [ $i -lt 10 ]
+        then
+	curl -O $(echo "ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/tl_2010_0"$i"_zcta510.zip")
+        unzip $(echo "tl_2010_0"$i"_zcta510.zip")
+	ogr2ogr -f "GeoJSON" $(echo "geojson/0"$i".json") $(echo "tl_2010_0"$i"_zcta510.shp")
+	topojson --id-property ZCTA5CE10 --out $(echo "topojson/0"$i".json") $(echo "geojson/0"$i".json")
+    else
+	curl -O $(echo "ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/tl_2010_"$i"_zcta510.zip")
+        unzip $(echo "tl_2010_"$i"_zcta510.zip")
+	ogr2ogr -f "GeoJSON" $(echo "geojson/"$i".json") $(echo "tl_2010_"$i"_zcta510.shp")
+	topojson --id-property ZCTA5CE10 --out $(echo "topojson/"$i".json") $(echo "geojson/"$i".json")
+        fi
+done
